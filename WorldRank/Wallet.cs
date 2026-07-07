2ea81d2db151794922f2133bed03b6a271ebac8b
Jason Argiantzis <jasonargiantzis@gmail.com>
2026-07-07 14:51:19 +0300

    Task 2 completed Created wallet

---
+++ WorldRank/Wallet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorldRank
+{
+    public class Wallet
+    {
+        public decimal Balance { get; private set; }
+        public Currency Currency { get;   set; }
+        public bool IsBlocked { get;  set; }
+
+        public Wallet(decimal initialBalance, Currency currency_const,bool isBlocked)
+        {
+            if (initialBalance < 0)
+                throw new ArgumentOutOfRangeException(nameof(initialBalance), "Initial balance must be non-negative.");
+            Balance = initialBalance;
+            Currency = currency_const;
+            IsBlocked = false;
+        }
+    }
+}
