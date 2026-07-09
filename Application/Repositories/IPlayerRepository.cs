849113c8bf613363d18240722fbbe38d89a98933
Jason Argiantzis <jasonargiantzis@gmail.com>
2026-07-09 14:44:42 +0300

    Application layer

---
+++ Application/Repositories/IPlayerRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entity;
+
+namespace Application.Repositories
+{
+	public interface IPlayerRepository
+	{
+		void AddPlayer(Player player);
+
+		IEnumerable<Player> GetAllPlayers();
+
+		void DeletePlayer(int playerId);
+
+		Player? FindPlayer(int playerId);
+
+		IEnumerable<IGrouping<int, Player>> GroupPlayersByScore();
+	}
+}
