053f2b17b36fe8536a09d7782980c9cde387f620
Jason Argiantzis <jasonargiantzis@gmail.com>
2026-07-09 10:30:23 +0300

    The stage of the Day 4 project

--- WorldRank/Currency.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace WorldRank
-{
-    public enum Currency { USD, EUR, GBP, JPY, AUD, CAD, CHF, CNY, SEK, NZD, NONE }
-}
