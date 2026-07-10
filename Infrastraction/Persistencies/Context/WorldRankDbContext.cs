34005f5b222c59bbb274bdaf085d47165aa0f751
Jason Argiantzis <jasonargiantzis@gmail.com>
2026-07-10 15:56:01 +0300

    Dependency Injection for db

---
+++ Infrastraction/Persistencies/Context/WorldRankDbContext.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Data.Entity;
+using System.Text;
+
+namespace Infrastructure.Persistencies.Context
+{
+    public partial class WorldRankDbContext : DbContext
+    {
+        public 
+    }
+}
